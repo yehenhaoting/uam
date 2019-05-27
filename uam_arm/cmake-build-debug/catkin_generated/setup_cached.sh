3af868b8e880583ca6093ad06577d8cb69116ecd
zm <yehenhaoting@sjtu.edu.cn>
2019-05-27 20:52:05 +0800

    add reconfigure cfg for servo test

--- uam_arm/cmake-build-debug/catkin_generated/setup_cached.sh
+++ uam_arm/cmake-build-debug/catkin_generated/setup_cached.sh
@@ -8,4 +8,5 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/zm/uam_ws/src/uam_arm/cmake-build-debug/devel:$CMAKE_PREFIX_PATH"
+export PYTHONPATH="/home/zm/catkin_moveit/devel/lib/python2.7/dist-packages:/home/zm/uam_ws/devel/lib/python2.7/dist-packages:/opt/ros/kinetic/lib/python2.7/dist-packages"
 export ROS_PACKAGE_PATH="/home/zm/uam_ws/src/uam_arm:$ROS_PACKAGE_PATH"
