48a350ddaf1754317838e8a8f7a65ed6841393bf
zm <yehenhaoting@sjtu.edu.cn>
2019-06-19 18:16:04 +0800

    test ok for float base

--- uam_arm/src/TRACIKsolver.cpp
+++ uam_arm/src/TRACIKsolver.cpp
@@ -20,6 +20,7 @@
 #include "sensor_msgs/JointState.h"
 #include <geometry_msgs/Quaternion.h>
 #include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseStamped.h>
 #include "std_msgs/String.h"
 #include <sstream>
 #include <string>
@@ -29,15 +30,26 @@ using namespace std;
 
 Vector EE_xyz = Vector(0.18, 0.0, 0.28);
 Rotation EE_rpy = Rotation::RPY(0.0, 1.5708, 0.0);
+
+KDL::Vector Base_xyz = KDL::Vector(0, 0, 0);
+KDL::Rotation Base_rot = KDL::Rotation::Quaternion(0, 0, 0, 1);
+
+
 bool joint_pub_flag = true;
 bool reset_flag = false;
 
 void pose_cb(const geometry_msgs::Pose& pose)
 {
-    EE_xyz = Vector(pose.position.x, pose.position.y, pose.position.z);
-    EE_rpy = Rotation::RPY(pose.orientation.x, pose.orientation.y, pose.orientation.z);
+    EE_xyz = KDL::Vector(pose.position.x, pose.position.y, pose.position.z);
+    EE_rpy = KDL::Rotation::RPY(pose.orientation.x, pose.orientation.y, pose.orientation.z);
     reset_flag = bool(pose.orientation.w);
 
+}
+
+void base_cb(const geometry_msgs::PoseStamped& msg)
+{
+    Base_xyz = KDL::Vector(msg.pose.position.x, msg.pose.position.y, msg.pose.position.z);
+    Base_rot = KDL::Rotation::Quaternion(msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w);
 }
 
 
@@ -47,7 +59,8 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "talker");
     ros::NodeHandle n;
     ros::Publisher joint_state_pub = n.advertise<sensor_msgs::JointState>("/joint_states", 1);
-    ros::Subscriber EEpose_sub = n.subscribe("/arm/cmd_pose", 10, pose_cb);
+    ros::Subscriber EE_pose_sub = n.subscribe("/arm/cmd_pose", 10, pose_cb);
+    ros::Subscriber Base_pose_sub = n.subscribe("/arm/base_pose", 10, base_cb);
     ros::Rate loop (50);
 
 
@@ -85,59 +98,24 @@ int main(int argc, char** argv)
     Tree my_tree;
     kdl_parser::treeFromParam("/robot_description",my_tree);
 
-//    Chain kdl_chain1;
-//    Chain kdl_chain2;
-//    Chain kdl_chain3;
-//    Chain kdl_chain4;
-//    Chain kdl_chain5;
-//    Chain kdl_chain6;
-//
-//    my_tree.getChain("base_link","link1",kdl_chain1);
-//    my_tree.getChain("base_link","link2",kdl_chain2);
-//    my_tree.getChain("base_link","link3",kdl_chain3);
-//    my_tree.getChain("base_link","link4",kdl_chain4);
-//    my_tree.getChain("base_link","link5",kdl_chain5);
-//    my_tree.getChain("base_link","link6",kdl_chain6);
-
     double eps=1E-3;
     int maxiter=1000;
     double eps_joints=1E-15;
 
-//    ChainFkSolverPos_recursive kdl_fksolver1 = ChainFkSolverPos_recursive(kdl_chain1);
-//    ChainFkSolverPos_recursive kdl_fksolver2 = ChainFkSolverPos_recursive(kdl_chain2);
-//    ChainFkSolverPos_recursive kdl_fksolver3 = ChainFkSolverPos_recursive(kdl_chain3);
-//    ChainFkSolverPos_recursive kdl_fksolver4 = ChainFkSolverPos_recursive(kdl_chain4);
-//    ChainFkSolverPos_recursive kdl_fksolver5 = ChainFkSolverPos_recursive(kdl_chain5);
-//    ChainFkSolverPos_recursive kdl_fksolver6 = ChainFkSolverPos_recursive(kdl_chain6);
-//
-//    unsigned int nj1 = kdl_chain1.getNrOfJoints();
-//    unsigned int nj2 = kdl_chain2.getNrOfJoints();
-//    unsigned int nj3 = kdl_chain3.getNrOfJoints();
-//    unsigned int nj4 = kdl_chain4.getNrOfJoints();
-//    unsigned int nj5 = kdl_chain5.getNrOfJoints();
-//    unsigned int nj6 = kdl_chain6.getNrOfJoints();
-//
-//    JntArray kdl_jointpositions1 = JntArray(nj1);
-//    JntArray kdl_jointpositions2 = JntArray(nj2);
-//    JntArray kdl_jointpositions3 = JntArray(nj3);
-//    JntArray kdl_jointpositions4 = JntArray(nj4);
-//    JntArray kdl_jointpositions5 = JntArray(nj5);
-//    JntArray kdl_jointpositions6 = JntArray(nj6);
-//
-//    Frame kdl_cartpos1;
-//    Frame kdl_cartpos2;
-//    Frame kdl_cartpos3;
-//    Frame kdl_cartpos4;
-//    Frame kdl_cartpos5;
-//    Frame kdl_cartpos6;
 
     sensor_msgs::JointState joint_state;
     unsigned int count = 0;
 
     while(ros::ok()){
-        KDL::Frame cartpos = Frame(EE_rpy, EE_xyz);
+//        KDL::Frame cartpos = Frame(EE_rpy, EE_xyz);
         KDL::Frame cartpos_init;
         KDL::JntArray result;
+
+
+        KDL::Frame F_world_baselink = KDL::Frame(Base_rot, Base_xyz);
+        KDL::Frame inv_F_world_baselink = F_world_baselink.Inverse();
+        KDL::Frame cartpos = inv_F_world_baselink * Frame(EE_rpy, EE_xyz);
+
 
         int kinematics_status;
         kinematics_status = tracik_solver.CartToJnt(nominal,cartpos,result);
@@ -178,41 +156,9 @@ int main(int argc, char** argv)
             }
             joint_pub_flag = true;
 
-
-//            for(unsigned int i=0;i<nj1;i++){
-//                kdl_jointpositions1(i)=result(i);
-//            }
-//            for(unsigned int i=0;i<nj2;i++){
-//                kdl_jointpositions2(i)=result(i);
-//            }
-//            for(unsigned int i=0;i<nj3;i++){
-//                kdl_jointpositions3(i)=result(i);
-//            }
-//            for(unsigned int i=0;i<nj4;i++){
-//                kdl_jointpositions4(i)=result(i);
-//            }
-//            for(unsigned int i=0;i<nj5;i++){
-//                kdl_jointpositions5(i)=result(i);
-//            }
-//            for(unsigned int i=0;i<nj6;i++){
-//                kdl_jointpositions6(i)=result(i);
-//            }
-//
-//            kdl_fksolver1.JntToCart( kdl_jointpositions1,kdl_cartpos1);
-//            kdl_fksolver2.JntToCart( kdl_jointpositions2,kdl_cartpos2);
-//            kdl_fksolver3.JntToCart( kdl_jointpositions3,kdl_cartpos3);
-//            kdl_fksolver4.JntToCart( kdl_jointpositions4,kdl_cartpos4);
-//            kdl_fksolver5.JntToCart( kdl_jointpositions5,kdl_cartpos5);
-//            kdl_fksolver6.JntToCart( kdl_jointpositions6,kdl_cartpos6);
-//            if(kdl_cartpos6.p.z()<-0.035&&kdl_cartpos5.p.z()<-0.035&&kdl_cartpos4.p.z()<-0.035&&kdl_cartpos3.p.z()<-0.035&&kdl_cartpos2.p.z()<-0.035)
-//            {
-//                joint_state_pub.publish(joint_state);
-//            }
-
         }
         else{ROS_ERROR_STREAM("Can NOT Solve!");}
 
-//        cout<<kdl_cartpos1.p<<"_"<<kdl_cartpos2.p<<"_"<<kdl_cartpos3.p<<"_"<<kdl_cartpos4.p<<"_"<<kdl_cartpos5.p<<"_"<<kdl_cartpos6.p<<endl;
         ros::spinOnce();
         loop.sleep();
         count ++;
---
+++ uam_arm/src/drone_tf_broadcaster.cpp
@@ -0,0 +1,45 @@
+//
+// Created by zm on 19-6-19.
+//
+
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Point.h>
+#include <geometry_msgs/Quaternion.h>
+
+
+int main(int argc, char** argv){
+    ros::init(argc, argv, "drone_tf_broadcaster");
+    ros::NodeHandle node;
+
+    tf::TransformBroadcaster br;
+    tf::Transform transform;
+    geometry_msgs::PoseStamped pose_drone;
+
+
+    ros::Publisher base_pub = node.advertise< geometry_msgs::PoseStamped >("arm/base_pose", 10);
+
+
+    ros::Rate rate(100.0);
+    while (node.ok()){
+        transform.setOrigin( tf::Vector3(-0.05 + 0.05*sin(ros::Time::now().toSec()), 0.05*cos(ros::Time::now().toSec()), -0.05) );
+        transform.setRotation( tf::Quaternion(0, 0, 0, 1) );
+
+
+
+        pose_drone.header.stamp = ros::Time::now();
+        pose_drone.pose.position.x = -0.05 + 0.05*sin(ros::Time::now().toSec());
+        pose_drone.pose.position.y = 0.05*cos(ros::Time::now().toSec());
+        pose_drone.pose.position.z = -0.05;
+        pose_drone.pose.orientation.x = 0;
+        pose_drone.pose.orientation.y = 0;
+        pose_drone.pose.orientation.z = 0;
+        pose_drone.pose.orientation.w = 1;
+
+        br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", "base_link"));
+        base_pub.publish(pose_drone);
+        rate.sleep();
+    }
+    return 0;
+}
